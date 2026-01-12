816977b180606adb077591156d0c65b4c60ef320
Tushar <tushar@Tushar-ka-MacBook-Pro.local>
2026-01-12 10:09:31 +0530

    fix embeddings

--- rag/embeddings/embedder.js
+++ rag/embeddings/embedder.js
@@ -1,8 +1,28 @@
-const crypto = require("crypto");
+const {pipeline} = require("@xenova/transformers")
+
+let embedder = null
+
+const loadModel = async() =>{
+    if(!embedder){
+        embedder = await pipeline(
+            "feature-extraction",
+            "Xenova/all-MiniLM-L6-v2"
+
+        )
+    }
+    return embedder
+}
+
 
 const embedText = async (text) => {
-  const hash = crypto.createHash("sha256").update(text).digest();
-  return Array.from(hash).map(b => b / 255);
+    const model = await loadModel()
+
+
+    const output = await model(text,{
+        pooling:'mean',
+        normalize:true
+    })
+  return Array.from(output.data)
 };
 
 module.exports = { embedText };
